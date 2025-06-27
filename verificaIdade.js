7562e502497a3fb1e5f3a0b92e3b24b43c8dacf9
Luis100000 <luis.dea@escola.pr.gov.br>
2025-06-27 14:53:14 -0300

    Create  verificaIdade.js

---
+++ verificaIdade.js
@@ -0,0 +1,9 @@
+// Criando a variável para armazenar a idade
+let idade = 17;  // Altere esse valor conforme necessário
+
+// Verificando se a idade é maior ou igual a 18
+if (idade >= 18) {
+    console.log("Você é maior de idade.");
+} else {
+    console.log("Você é menor de idade.");
+}
