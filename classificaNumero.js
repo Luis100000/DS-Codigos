ff61006dd6f62f75f9c66e70a7c961e0c5fafb1c
Luis100000 <luis.dea@escola.pr.gov.br>
2025-06-27 14:58:15 -0300

    Create classificaNumero.js

---
+++ classificaNumero.js
@@ -0,0 +1,11 @@
+// Criando a variável para armazenar o número
+let numero = -5;  // Você pode mudar esse valor para testar outros casos
+
+// Verificando se o número é positivo, negativo ou zero
+if (numero > 0) {
+    console.log("O número é positivo.");
+} else if (numero < 0) {
+    console.log("O número é negativo.");
+} else {
+    console.log("O número é zero.");
+}
