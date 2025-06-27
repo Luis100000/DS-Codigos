d8c908a8c5779458c9252493c0cf9967d0b1b3f2
Luis100000 <luis.dea@escola.pr.gov.br>
2025-06-27 14:57:06 -0300

    Create  verificaSenha.js

---
+++ verificaSenha.js
@@ -0,0 +1,12 @@
+// Armazenando a senha correta
+let senha_correta = "12345";
+
+// Armazenando a senha digitada pelo usuário
+let senha_digitada = "12345"; // Altere para testar a verificação
+
+// Verificando se a senha está correta
+if (senha_digitada === senha_correta) {
+    console.log("Login bem-sucedido!");
+} else {
+    console.log("Senha incorreta. Tente novamente.");
+}
