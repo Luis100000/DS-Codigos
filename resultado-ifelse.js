bfa1466e65d7bd08fe99913915b75afdcdd62839
Luis100000 <luis.dea@escola.pr.gov.br>
2025-07-04 14:50:15 -0300

    Create resultado-ifelse.js

---
+++ resultado-ifelse.js
@@ -0,0 +1,8 @@
+
+let avaliacao = ???; // Ex. de nota pode ter casa decimal assim 0.00 - não use vírgula!
+
+if (variável?> ???) { //  Incluir a nota de aprovação
+  console.log("Você está aprovado. Parabéns!");
+} else {
+  comando?("Infelizmente ??? ."); // Incluir msg da última condição
+}
