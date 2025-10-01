b166bfbc499eefd9c80f5a138596035bad07d339
Luis100000 <luis.dea@escola.pr.gov.br>
2025-10-01 14:50:29 -0300

    Create  jogo-da-forca1.py

---
+++ jogo-da-forca1.py
@@ -0,0 +1,33 @@
+1 Jogo do Forco
+2 palavra "python"
+3 letras_descobertas [""] (palavra)
+4 tentativas = 6
+5 letras_usadas []
+6
+7 print(" Jogo da Forca! Adivinhe a palavra letra por letra.")
+9- while tentativas > 0:
+10
+print("\nPalavra:", "".(letras_descobertas))
+11
+letra("Digite una letra (ou 'sair para desistir): ").or()
+12
+13
+If letra "sair":
+14
+print("você desistiu do jogo.")
+15
+break Interrompe o Loop
+16
+17
+if not letra.iselens() or le (letra) = 1:
+18
+print("Entrada inválida. Digite apenas uma letra.")
+19
+continue Pula para a próxima iteração
+20
+21-
+If letra in letras usadas:
+22
+print("você já tentou essa letra.")
+23
+pass Não faz nada, apenas segue o flwin
