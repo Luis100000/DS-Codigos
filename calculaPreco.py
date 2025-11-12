c9a0e3f0fb9a57a5a509acfec39214dcb4c844d2
Luis100000 <luis.dea@escola.pr.gov.br>
2025-11-12 14:44:47 -0300

    Create calculaPreco.py

---
+++ calculaPreco.py
@@ -0,0 +1,5 @@
+def calcular_preco_final (nome preço. desconto = 0):
+= * preço_final preço ( preço desconto / 100)
+print (f"Preço inicial de { nome }: R$ { preço: 2f}")
+print (f"Preço final de { nome}: R$ { preco_final 2 f}")
+calcular_preco_final ("Fone de ouvido" 200. .25)
